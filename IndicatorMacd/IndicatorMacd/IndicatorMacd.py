b4ef733810557935690a9f3e84c6ce63aacb647d
Blohh <52373144+Blohh@users.noreply.github.com>
2020-03-08 12:04:48 +0100

    Add MACD class with calculateMACD method

--- IndicatorMacd/IndicatorMacd/IndicatorMacd.py
+++ IndicatorMacd/IndicatorMacd/IndicatorMacd.py
@@ -2,19 +2,34 @@ import pandas as pd
 import numpy as np
 
 class Currency:
-    __name = ""
+    name = ""
     course=[]
     date=[]
     def __init__(self, name):
-        self.__name=name
+        self.name=name
     def getName(self):
-        return __name
+        return name
     def createData(self):
-        filename="./data/"+str(self.__name)+".csv"
+        filename="./data/"+str(self.name)+".csv"
         data=pd.read_csv(str(filename),delimiter=",")
         course=data["Course"]
         date=data["Date"]
         return date, course
+
+class MACD:
+    macd=[]
+    signal=[]
+    def __init__(self, course, date):
+        calculateMACD(course)
+        calculateSIGNAL(course)
+    def calculateMACD(course):
+        for i in range(len(course)):
+            if i>=26:
+                ema12=calculateEMA(course, i, 12)
+                ema26=calculateEMA(course, i, 26)
+                macd.append(ema12-ema26)
+            else:
+                macd.append(0.0)
 
 
 
