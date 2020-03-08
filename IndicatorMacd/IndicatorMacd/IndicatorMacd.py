6dbbba4f100c4b4d59fd5929fa6094ff2321456f
Blohh <52373144+Blohh@users.noreply.github.com>
2020-03-08 11:47:07 +0100

    Create currency class to get important data from .csv files

--- IndicatorMacd/IndicatorMacd/IndicatorMacd.py
+++ IndicatorMacd/IndicatorMacd/IndicatorMacd.py
@@ -1,1 +1,24 @@
+import pandas as pd
+import numpy as np
 
+class Currency:
+    __name = ""
+    course=[]
+    date=[]
+    def __init__(self, name):
+        self.__name=name
+    def getName(self):
+        return __name
+    def createData(self):
+        filename="./data/"+str(self.__name)+".csv"
+        data=pd.read_csv(str(filename),delimiter=",")
+        course=data["Course"]
+        date=data["Date"]
+        return date, course
+
+
+
+if __name__ == '__main__':
+    currency = Currency("jen_japonski")
+    date, course=currency.createData()
+    
